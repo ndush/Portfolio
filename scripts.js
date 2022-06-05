9d167346bfd1db0edb27b96c16d28a3e613f2df5
damaris <mdamarisnduku@gmail.com>
2022-06-05 21:35:21 +0300

    added javascript

--- scripts.js
+++ scripts.js
@@ -1,2 +1,37 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+function validate(){
+    const firstname = document.getElementById("firstname").value;
+    const lastname = document.getElementById("lastname").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    
+    const error_message = document.getElementById("error_message");
+    
+    error_message.style.padding = "10px";
+    
+    let text;
+    if(firstname.length < 5){
+      text = "Please Enter valid First Name";
+      error_message.innerHTML = text;
+      return false;
+    }
+
+    if(lastname.length < 5){
+        text = "Please Enter valid Last Name";
+        error_message.innerHTML = text;
+        return false;
+      }
+     
+      if(email.indexOf("@") == -1 || email.length < 6){
+        text = "Please Enter valid Email";
+        error_message.innerHTML = text;
+        return false;
+      }  
+   
+      if(subject.length <= 20){
+      text = "Please Enter More Than 20 Characters";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
+  }
